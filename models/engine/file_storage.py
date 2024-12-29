cd804111c2910cdeeaf820c72860967cefb12561
7asanka <hassan2002_kimo@hotmail.com>
2024-12-29 08:42:44 -0800

    hbnb task 5

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -8,9 +8,13 @@ class FileStorage:
     __file_path = 'file.json'
     __objects = {}
 
-    def all(self):
+    def all(self, cls=None):
         """Returns a dictionary of models currently in storage"""
-        return FileStorage.__objects
+        if cls is None:
+            return FileStorage.__objects
+
+        return {key: obj for key, obj in FileStorage.__objects.items()
+                if isinstance(obj, cls)}
 
     def new(self, obj):
         """Adds new object to storage dictionary"""
@@ -24,6 +28,12 @@ class FileStorage:
             for key, val in temp.items():
                 temp[key] = val.to_dict()
             json.dump(temp, f)
+
+    def delete(self, obj=None):
+        """delete obj from __objects"""
+        if obj:
+            key = obj.to_dict()['__class__'] + '.' + obj.id
+            FileStorage.__objects.pop(key, None)
 
     def reload(self):
         """Loads storage dictionary from file"""
@@ -45,6 +55,6 @@ class FileStorage:
             with open(FileStorage.__file_path, 'r') as f:
                 temp = json.load(f)
                 for key, val in temp.items():
-                        self.all()[key] = classes[val['__class__']](**val)
+                    self.all()[key] = classes[val['__class__']](**val)
         except FileNotFoundError:
             pass
