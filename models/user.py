4736351f89a6d073669ded01f000aa5d1b6925ff
7asanka <hassan2002_kimo@hotmail.com>
2024-12-31 19:36:46 +0100

    models/user.py adjust

--- models/user.py
+++ models/user.py
@@ -15,7 +15,7 @@ class User(BaseModel, Base):
     last_name = Column(String(128), nullable=True)
 
     places = relationship("Place", backref="user",
-                          cascade="all, delete, delete-orphan")
+                          cascade="delete")
 
     reviews = relationship("Review", backref="user",
-                           cascade="all, delete, delete-orphan")
+                           cascade="delete")
